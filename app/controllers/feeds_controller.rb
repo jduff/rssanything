4b115aee9b1b52319cf3c3bd2d19fb4927d96054
John Duff <john@john-duffs-macbook-pro-2.local>
2008-05-11 23:22:14 -0400

    broken

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -5,6 +5,7 @@ class FeedsController < ApplicationController
   end
   
   def new
+    pp params
     
     feed = Feed.new(:title => params[:url].gsub('[', '[@'), :link => params[:url].gsub('[', '[@'), 
             :link_regexp => params[:link].gsub('[', '[@'), :title_regexp => params[:title].gsub('[', '[@'), 
@@ -18,10 +19,23 @@ class FeedsController < ApplicationController
   def show
     @feed = Feed.find_by_id(params[:id])
     
-    Feed.async_send :refresh, params[:id]
-    #Feed.send :refresh, params[:id]
+    parsed_items = @feed.execute
+
+    existing_items = Item.find(:all, 
+      :conditions => ["feed_id =? and guid in (?)", @feed.id, parsed_items.collect(&:guid)])
+
+
+    existing_items.collect!(&:guid) unless existing_items.empty?
+
+    new_items = parsed_items.reject {|item| existing_items.include?(item.guid)}
+
+    @feed.items << new_items unless new_items.empty?
+
+    @feed.last_published = Time.now
+
+    @feed.save unless new_items.empty?
     
-    @items = Item.find(:all, :conditions => ["feed_id = ?", params[:id]], 
+    @items = Item.find(:all, :conditions => ["feed_id = ?", @feed.id], 
       :order => "created_at DESC")
     
     respond_to do |format|
